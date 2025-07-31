8e07191324ea4c285600cd6ba2eb9ee0b0dca58f
injustice <injustice@users.noreply.github.com>
2025-07-31 14:26:59 +0530

    render markdown edit

--- nvim/.config/nvim/lua/main/settings.lua
+++ nvim/.config/nvim/lua/main/settings.lua
@@ -1,26 +1,44 @@
+-- ========================
+-- Core Settings
+-- ========================
 vim.opt.mouse = "a"
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.clipboard = "unnamedplus"
 
+-- Search
 vim.opt.showmatch = true
 vim.opt.ignorecase = true
 vim.opt.wrap = true
-
-vim.opt.termguicolors = true
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.expandtab = true
-vim.opt.autoindent = true
-vim.opt.shiftwidth = 4
+-- Colors & Cursor
+vim.opt.termguicolors = true
+vim.opt.cursorline = true
+vim.opt.signcolumn = "yes"
 
+-- Tabs: 8-space tabs for all files
+vim.opt.tabstop = 8 -- Visual width of tab
+vim.opt.softtabstop = 8 -- Editing width of tab
+vim.opt.shiftwidth = 8 -- Indentation width
+vim.opt.expandtab = false -- Use real tabs (change to true for spaces)
+vim.opt.autoindent = true
+
+-- Performance & Undo
 vim.opt.updatetime = 300
 vim.opt.wildmode = "longest,list"
-vim.opt.cursorline = true
-
 vim.opt.undofile = true
 vim.opt.undodir = vim.fn.stdpath("cache") .. "/undo"
-vim.opt.signcolumn = "yes"
+
+-- ========================
+-- Autoformat on Save using LSP
+-- ========================
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function()
+		pcall(function()
+			vim.lsp.buf.format({ async = false })
+		end)
+	end,
+})
--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -10,9 +10,30 @@ return {
 			render_modes = { "n", "c", "t" },
 			anti_conceal = {
 				enabled = true,
-				ignore = { "RenderMarkdownMath", "check_icon" },
+				disabled_modes = false,
 				above = 0,
 				below = 0,
+				-- Which elements to always show, ignoring anti conceal behavior. Values can either be
+				-- booleans to fix the behavior or string lists representing modes where anti conceal
+				-- behavior will be ignored. Valid values are:
+				--   bullet
+				--   callout
+				--   check_icon, check_scope
+				--   code_background, code_border, code_language
+				--   dash
+				--   head_background, head_border, head_icon
+				--   indent
+				--   link
+				--   quote
+				--   sign
+				--   table_border
+				--   virtual_lines
+				ignore = {
+					code_background = true,
+					indent = true,
+					sign = true,
+					virtual_lines = true,
+				},
 			},
 			heading = {
 				width = "block",
@@ -61,13 +82,17 @@ return {
 				},
 				highlight = "RenderMarkdownLink",
 			},
-			checkbox = { -- fixed nesting per GitHub issue #38
-				unchecked = { icon = "✘ " },
-				checked = { icon = "✔ " },
-				custom = { todo = { rendered = "◯ " } },
-				right_pad = 1, -- for spacing alignment
-				bullet = true, -- optionally render bullet even with checkbox (#423)
-			},
+			require("render-markdown").setup({
+				checkbox = {
+					custom = {
+						important = {
+							raw = "[~]",
+							rendered = "󰓎 ",
+							highlight = "DiagnosticWarn",
+						},
+					},
+				},
+			}),
 			preset = "none", -- avoid overriding defaults unexpectedly (#3)
 			max_file_size = 10.0,
 			debounce = 100,
