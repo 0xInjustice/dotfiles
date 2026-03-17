d299bcf2e4982735b7614914d86a9d3bfbca44bd
0xinjustice <0xinjustice@users.noreply.github.com>
2026-03-17 12:51:15 +0530

    created a colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -3,35 +3,35 @@ local M = {}
 
 M.setup = function()
 	local colors = {
-		bg = "#000000", -- pure black background
+		bg = "#22252a", -- void black
 		fg = "#ffffff", -- bright white foreground
-		red = "#ff5555",
+		red = "#ff3b3b", -- mars red
 		green = "#50fa7b",
-		yellow = "#f1fa8c",
-		blue = "#6272a4",
-		magenta = "#ff79c6",
+		pink = "#af46f5", -- heliotrope
+		blue = "#32a6d5", -- scooter
+		portage = "#9ea3f3", -- portage
 		cyan = "#8be9fd",
-		white = "#f8f8f2",
+		white = "#dde3ea", -- drift silver
 	}
 
 	vim.cmd("highlight clear")
 	vim.o.termguicolors = true
-	vim.o.background = "dark"
+	-- vim.o.background = "dark"
 
 	-- Core highlights
 	vim.api.nvim_set_hl(0, "Normal", { fg = colors.fg, bg = colors.bg })
 	vim.api.nvim_set_hl(0, "Comment", { fg = colors.green, italic = true })
 	vim.api.nvim_set_hl(0, "String", { fg = colors.cyan })
-	vim.api.nvim_set_hl(0, "Keyword", { fg = colors.magenta, bold = true })
+	vim.api.nvim_set_hl(0, "Keyword", { fg = colors.portage, bold = true })
 	vim.api.nvim_set_hl(0, "Function", { fg = colors.blue })
-	vim.api.nvim_set_hl(0, "Type", { fg = colors.yellow })
+	vim.api.nvim_set_hl(0, "Type", { fg = colors.pink, bold = true })
 	vim.api.nvim_set_hl(0, "Constant", { fg = colors.red })
 	vim.api.nvim_set_hl(0, "Identifier", { fg = colors.white })
 
 	-- Markdown highlights
-	vim.api.nvim_set_hl(0, "markdownH1", { fg = colors.magenta, bold = true })
-	vim.api.nvim_set_hl(0, "markdownH2", { fg = colors.magenta, bold = true })
-	vim.api.nvim_set_hl(0, "markdownH3", { fg = colors.magenta, bold = true })
+	vim.api.nvim_set_hl(0, "markdownH1", { fg = colors.portage, bold = true })
+	vim.api.nvim_set_hl(0, "markdownH2", { fg = colors.portage, bold = true })
+	vim.api.nvim_set_hl(0, "markdownH3", { fg = colors.portage, bold = true })
 	vim.api.nvim_set_hl(0, "markdownLinkText", { fg = colors.cyan, underline = true })
 	vim.api.nvim_set_hl(0, "markdownBlockquote", { fg = colors.green, italic = true })
 end
